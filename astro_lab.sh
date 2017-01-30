4b67d12b9d07613886c4de6ba4463958fccb0701
Maxime Delorme <Maxime.Delorme@gmail.com>
2017-01-30 11:06:50 +0000

    Using default ds9 instead of mine

--- astro_lab.sh
+++ astro_lab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PYTHONPATH=/user/phstf/md0046/tools/photutils/build/lib.linux-x86_64-2.7/:/user/phstf/md0046/tools/pyds9/build/lib.linux-x86_64-2.7:$PYTHONPATH
-export PATH=/user/phstf/md0046/tools/xpa/build/bin:/user/phstf/md0046/tools/ds9:$PATH
+export PATH=/user/phstf/md0046/tools/xpa/build/bin:$PATH #/user/phstf/md0046/tools/ds9:$PATH
 export LD_LIBRARY_PATH=/user/phstf/md0046/tools/libs:$LD_LIBRARY_PATH
 
 ipython -i astro_lab.py
